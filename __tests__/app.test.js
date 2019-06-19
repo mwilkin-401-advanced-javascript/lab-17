0e3837e160df8f4daabde072b2d6c0eff8cce936
Matt Wilkin <mwilkin22@gmail.com>
2019-06-18 21:44:30 -0700

    add test for convertCase.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const app = require('../app.js');
+const server = require('../server.js');
+
+describe('convertCase', () => {
+  it('should convert a string to uppercase', () => {
+    let string = 'test string';
+    let convertedString = app.convertCase(string);
+    expect(convertedString).toEqual('TEST STRING');
+  });
+});
